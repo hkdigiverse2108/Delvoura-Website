bc80d5680e7f0cf8048e1f19f3e1a49d28fde5ec
HK-Bhupendra <bhupendrahkdigiverse@gmail.com>
2026-03-17 11:20:08 +0530

    pwa setup and folder structure setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello , Delvoura !</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,65 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const PORT = Number(env.VITE_PORT) || 5173;
+  const API_BASE_URL = env.VITE_API_BASE_URL || "http://localhost:3000";
+
+  // https://vite.dev/config/
+  return {
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: "prompt",
+        injectRegister: false,
+
+        pwaAssets: {
+          disabled: false,
+          config: true,
+        },
+
+        manifest: {
+          name: "Delvoura",
+          short_name: "Delvoura",
+          description: "Delvoura App",
+          theme_color: "#sd3bff",
+        },
+
+        workbox: {
+          maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
+          globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+          cleanupOutdatedCaches: true,
+          clientsClaim: true,
+        },
+
+        devOptions: {
+          enabled: true,
+          navigateFallback: "index.html",
+          suppressWarnings: true,
+          type: "module",
+        },
+      }),
+    ],
+    server: {
+      host: true,
+      allowedHosts: true,
+      port: PORT,
+      proxy: {
+        "/api": {
+          target: API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+    preview: {
+      port: PORT,
+      strictPort: true,
+    },
+    define: {
+      "process.env.VITE_API_BASE_URL": JSON.stringify(API_BASE_URL),
+    },
+  };
+});
